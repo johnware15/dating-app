0fd7d4e43ca0d563d14f0835baaee03adb9b3c88
John Ware <johnware@Johns-MacBook-Air.local>
2017-09-05 13:55:24 -0700

    File system setup

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS dating_app;
+CREATE DATABASE dating_app;
+
+\c dating_app
+
+CREATE TABLE "users" (
+"user_id"  SERIAL NOT NULL,
+"email" VARCHAR NOT NULL,
+"password" VARCHAR(250) NOT NULL,
+"face_image" VARCHAR(250) NOT NULL,
+"body_image" VARCHAR(250) NOT NULL DEFAULT 'NULL',
+"description" VARCHAR(500) NOT NULL DEFAULT 'NULL',
+"message" VARCHAR NOT NULL,
+"crushes" VARCHAR NOT NULL,
+"fans" VARCHAR NOT NULL,
+PRIMARY KEY ("user_id")
+);
+
+CREATE TABLE "crush" (
+"user_id" SERIAL NOT NULL,
+"fan_ids" VARCHAR NOT NULL,
+"crush_ids" VARCHAR NOT NULL,
+PRIMARY KEY ("user_id")
+);
+
+ALTER TABLE "crush" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
